0b2939d398349d0c8e755504d02b92aba6fff6cb
Jacob Miller <158308043+Jamiller137@users.noreply.github.com>
2025-07-04 17:43:23 -0500

    initual new version

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "My personal website dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Web development tools
+            nodejs_24
+            nodePackages.live-server
+            nodePackages.prettier
+            nodePackages.eslint
+
+            # Static site generator
+            # hugo
+
+            git
+            gh
+          ];
+
+          shellHook = ''
+            echo "Jacob Miller's website dev environment loaded!"
+            echo "Available commands:"
+            echo "  live-server src/    - Start local dev server"
+            echo "  prettier --write .  - Format code"
+            echo "  eslint src/         - Lint JavaScript"
+            echo ""
+          '';
+        };
+      });
+}
+
