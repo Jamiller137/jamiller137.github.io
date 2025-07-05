4df455cd274b4dbfc72dd541157dee2f415a25b7
Jacob Miller <158308043+Jamiller137@users.noreply.github.com>
2025-07-05 17:20:59 -0500

    update base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/jamiller137.github.io",
+  base: "/",
   build: {
     outDir: "dist",
   },
